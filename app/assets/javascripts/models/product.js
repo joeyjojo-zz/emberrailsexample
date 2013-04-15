0323358435f8f30f841474a66c114d757d3939e0
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-04-15 22:32:32 +0100

    Added fixture data for products

--- app/assets/javascripts/models/product.js
+++ app/assets/javascripts/models/product.js
@@ -4,3 +4,14 @@ Sampleapp.Product = DS.Model.extend({
     image_url: DS.attr('string'),
     price: DS.attr('number')
 });
+
+
+Sampleapp.Product.FIXTURES = [
+    {
+        id: 1,
+        title:"Sample product",
+        description:"This is a test description",
+        image_url:"sample.jpg",
+        price:1.99
+    }
+];
