23d1d54760747cd5c7e56f8e2c4520610ee6d1c9
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-05-01 22:46:26 +0100

    Added product routes to router

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,4 +3,8 @@ App.Router.map(function() {
     this.route('new');
     this.resource('contact', {path: ':contact_id'});
   });
+  this.resource('products', function() {
+      this.route('new');
+      this.resource('product', {path: ':product_id'});
+  })
 });
