6aae579d94bbd161da5c38908cb2eb895eedf9b5
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-05-01 22:43:54 +0100

    Added remainder of the rails files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120924225330) do
+ActiveRecord::Schema.define(:version => 20130501212344) do
 
   create_table "contacts", :force => true do |t|
     t.string   "first_name"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(:version => 20120924225330) do
     t.integer  "contact_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "text"
+    t.string   "image_url"
+    t.decimal  "price"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
